cc02a9625ac815e8ac2450b2c51cb0da5c66666b
Maciej Koczot <mefiuea@gmail.com>
2022-09-18 09:03:20 +0200

    upgrade models

--- cars_communicator_app/models.py
+++ cars_communicator_app/models.py
@@ -54,66 +54,66 @@ class RegisteredCars(models.Model):
         return f'Registered car: {self.registration_number}'
 
 
-# class MyUserManager(BaseUserManager):
-#     def create_user(self, email, user_name, password=None):
-#         if not email:
-#             raise ValueError('User must provide email.')
-#         if not user_name:
-#             raise ValueError('User must provide user name.')
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, user_name, password=None):
+        if not email:
+            raise ValueError('User must provide email.')
+        if not user_name:
+            raise ValueError('User must provide user name.')
 
-#         user = self.model(
-#             email=self.normalize_email(email),
-#             user_name=user_name,
-#         )
+        user = self.model(
+            email=self.normalize_email(email),
+            user_name=user_name,
+        )
 
-#         user.set_password(password)
-#         user.save(using=self._db)
+        user.set_password(password)
+        user.save(using=self._db)
 
-#         return user
+        return user
 
-#     def create_superuser(self, email, user_name, password=None):
-#         user = self.create_user(
-#             email=self.normalize_email(email),
-#             user_name=user_name,
-#             password=password
-#         )
-#         user.is_admin = True
-#         user.is_staff = True
-#         user.is_active = True
-#         user.is_superuser = True
-#         user.is_email_verified = True
-#         user.save(using=self._db)
+    def create_superuser(self, email, user_name, password=None):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            user_name=user_name,
+            password=password
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_active = True
+        user.is_superuser = True
+        user.is_email_verified = True
+        user.save(using=self._db)
 
-#         return user
+        return user
 
 
-# class MyUser(AbstractBaseUser, PermissionsMixin):
-#     user_name = models.CharField(
-#         max_length=30, unique=False, verbose_name='user name')
-#     first_name = models.CharField(
-#         max_length=30, unique=False, verbose_name='first name')
-#     last_name = models.CharField(
-#         max_length=30, unique=False, verbose_name='last name')
-#     email = models.EmailField(
-#         max_length=100, unique=True, verbose_name='email')
-#     phone_number = models.CharField(
-#         max_length=17, unique=True, blank=False, validators=[phone_regex])
-#     cars = models.ManyToManyField(
-#         'RegisteredCars', related_name='user_registeredcars')
-#     is_email_verified = models.BooleanField(default=False)
-#     date_joined = models.DateTimeField(
-#         verbose_name='date joined', auto_now_add=True)
-#     last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
-#     is_admin = models.BooleanField(default=False)
-#     is_active = models.BooleanField(default=False)
-#     is_staff = models.BooleanField(default=False)
-#     is_superuser = models.BooleanField(default=False)
+class MyUser(AbstractBaseUser, PermissionsMixin):
+    user_name = models.CharField(
+        max_length=30, unique=False, verbose_name='user name')
+    first_name = models.CharField(
+        max_length=30, unique=False, verbose_name='first name')
+    last_name = models.CharField(
+        max_length=30, unique=False, verbose_name='last name')
+    email = models.EmailField(
+        max_length=100, unique=True, verbose_name='email')
+    phone_number = models.CharField(
+        max_length=17, unique=True, blank=False, validators=[phone_regex])
+    cars = models.ManyToManyField(
+        'RegisteredCars', related_name='user_registeredcars')
+    is_email_verified = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(
+        verbose_name='date joined', auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = []
-#     # REQUIRED_FIELDS = ['user_name', ]
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    # REQUIRED_FIELDS = ['user_name', ]
 
-#     objects = MyUserManager()
+    objects = MyUserManager()
 
-#     def __str__(self):
-#         return self.email
+    def __str__(self):
+        return self.email
